49b9629d446b88fa8667453598f0573ffa0582ef
ilevd <ilevd@mail.ru>
2016-09-28 17:12:01 +0700

    First commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to playground-samples-parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject com.anychart/playground-samples-parser "0.1.0"
+  :description "Anychart samples parsing library"
+  :url "https://github.com/AnyChart/playground-samples-parser"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 ;; html
+                 [enlive "1.1.6"]
+                 ;; logging
+                 [com.taoensso/timbre "4.3.1"]])
