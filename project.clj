6fb437c1c4e7dde642d66ee80b09c4ccc1a98a7e
ilevd <ilevd@mail.ru>
2016-09-28 19:39:57 +0700

    Fix project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject com.anychart/playground-samples-parser "0.1.2"
+(defproject com.anychart/playground-samples-parser "0.1.2-SNAPSHOT"
   :description "Anychart samples parsing library"
   :url "https://github.com/AnyChart/playground-samples-parser"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;; html
                  [enlive "1.1.6"]
