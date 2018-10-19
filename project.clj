8f4dc99feafb517ba4f638f29b45aee093978107
ilevd <ilevd@mail.ru>
2018-10-19 14:15:46 +0700

    Update version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.anychart/playground-samples-parser "0.2.5"
+(defproject com.anychart/playground-samples-parser "0.2.6"
   :description "Anychart samples parsing library"
   :url "https://github.com/AnyChart/playground-samples-parser"
   :license {:name "MIT License"
